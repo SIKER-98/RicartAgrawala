f811a291b2de98f34136811b1a72a58a57d6054e
SIKER-98 <hubert.sikorski@yahoo.com>
2024-01-04 16:44:14 +0100

    Wersja z GUI

--- src/GUI.py
+++ src/GUI.py
@@ -1,55 +1,109 @@
-import openpyxl
+import sys
+import threading
+import tkinter as tk
+from multiprocessing import Process
+from threading import Thread
 from tkinter import *
-from tkinter import messagebox
+from RicartAgrawala import RicartAgrawala
 
 
-def register():
-    # Get the user input from the form
-    first_name = first_name_entry.get()
-    Last_name = Last_name_entry.get()
-    email = email_entry.get()
-    Mobile = Mobile_entry.get()
+class GUI:
 
-    # Create a new row with the user input
-    new_row = [first_name, Last_name, email,Mobile]
+    def __init__(self, my_id):
+        self.ra = None
+        self.my_id = my_id
 
-    # Append the new row to the Excel sheet
-    workbook = openpyxl.load_workbook("registration_data.xlsx")
-    sheet = workbook.active
-    sheet.append(new_row)
-    workbook.save("registration_data.xlsx")
-    messagebox.showinfo("Success", "Registration successful!")
+        self.root = tk.Tk()
+
+        self.addresses = tk.StringVar()
+        self.ports = tk.StringVar()
+        self.numberOfRequest = tk.IntVar()
+
+        self.lock = threading.Lock()
+
+        self.requests = []
+        self.history = []
+
+        self.addresses_label = tk.Label(self.root, text="Addresses:", font=('calibre', 10, 'bold'))
+        self.addresses_entry = tk.Entry(self.root, textvariable=self.addresses, font=('calibre', 10, 'normal'))
+        self.addresses_entry.insert(0, '192.168.0.245 192.168.0.163 192.168.0.10')
+
+        self.port_label = tk.Label(self.root, text="Ports:", font=('calibre', 10, 'bold'))
+        self.port_entry = tk.Entry(self.root, textvariable=self.ports, font=('calibre', 10, 'normal'))
+        self.port_entry.insert(0, "5000 5001 5002")
+
+        self.request_label = tk.Label(self.root, text="Request:", font=('calibre', 10, 'bold'))
+        self.request_entry = tk.Entry(self.root, textvariable=self.numberOfRequest, font=('calibre', 10, 'normal'))
+        self.request_entry.insert(0, '3')
+
+        self.run_button = tk.Button(self.root, text="RUN", command=self.run_algorithm)
+        self.clear_button = tk.Button(self.root, text="CLEAR HISTORY", command=self.clear_history)
+        self.request_button = tk.Button(self.root, text="REQUEST", command=self.send_request)
+
+        self.data = tk.Listbox(self.root)
+        self.data = tk.Listbox(self.root, width=70, height=15)
+        self.scrollbar = tk.Scrollbar(self.root)
+        self.data.config(yscrollcommand=self.scrollbar.set)
+
+        self.addresses_label.grid(row=0, column=0, sticky='nesw')
+        self.addresses_entry.grid(row=0, column=1, sticky='nesw')
+        self.port_label.grid(row=1, column=0, sticky='nesw')
+        self.port_entry.grid(row=1, column=1, sticky='nesw')
+        self.request_label.grid(row=2, column=0, sticky='nesw')
+        self.request_entry.grid(row=2, column=1, sticky='nesw')
+
+        self.run_button.grid(row=3, column=0, columnspan=2, sticky='nesw')
+        self.clear_button.grid(row=4, column=0, columnspan=2, sticky='nesw')
+        self.request_button.grid(row=5, column=0, columnspan=2, sticky='nesw')
+
+        self.data.grid(row=6, column=0, columnspan=2, sticky='nsew')
+        self.data.columnconfigure(0, weight=1)
+
+        self.scrollbar.grid(row=6, column=2, sticky='ns')
+
+    def run_algorithm(self):
+        addresses = self.addresses_entry.get().split(' ')
+        port = [int(port) for port in self.port_entry.get().split(' ')]
+
+        print(addresses)
+        print(port)
+
+        self.ra = RicartAgrawala(self.my_id, addresses, port, self)
+
+    def send_request(self):
+        process = Thread(target=self.ra.request_cs, args=(self.request_entry.get(),))
+        process.start()
+
+    def clear_history(self):
+        self.data.delete(0, 'end')
+
+    def log_listen(self,clock, from_id):
+        self.lock.acquire()
+        log = f"Clock: {clock} - LISTEN: {from_id}"
+        print(log)
+        self.data.insert('end', log)
+        self.lock.release()
+
+    def log_receive(self, clock, from_id, type):
+        self.lock.acquire()
+        log = f"Clock: {clock} - Receive {type}: {from_id}"
+        print(log)
+        self.data.insert('end', log)
+        self.data.itemconfig(len(self.data.get(0, 'end')) - 1, {'bg': '#F06F26'})
+        self.lock.release()
+
+    def log_cs(self, clock, type):
+        self.lock.acquire()
+        log = f"Clock: {clock} - {type} CS"
+        print(log)
+        self.data.insert('end', log)
+        self.data.itemconfig(len(self.data.get(0, 'end')) - 1, {'bg': '#F013ED'})
+        self.lock.release()
+
+    def start(self):
+        self.root.mainloop()
 
 
-# Create the main tkinter window
-root = Tk()
-root.title("Registration Form")
-root.geometry('300x300')
-
-# Create labels and entry fields for each input
-first_name_label = Label(root, text="First Name:")
-first_name_label.pack()
-first_name_entry = Entry(root)
-first_name_entry.pack()
-
-Last_name_label = Label(root, text="Last Name:")
-Last_name_label.pack()
-Last_name_entry = Entry(root)
-Last_name_entry.pack()
-
-email_label = Label(root, text="Email:")
-email_label.pack()
-email_entry = Entry(root)
-email_entry.pack()
-
-Mobile_label = Label(root, text="Mobile:")
-Mobile_label.pack()
-Mobile_entry = Entry(root)
-Mobile_entry.pack()
-
-
-
-register_button = Button(root, text="Register", command=register)
-register_button.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    gui = GUI(int(sys.argv[1]))
+    gui.start()
--- src/RicartAgrawala.py
+++ src/RicartAgrawala.py
@@ -1,21 +1,14 @@
-import queue
-import socket
 import threading
+import socket
 import time
-import sys
-import tkinter
-from tkinter import *
-from tkinter import scrolledtext
-
-# Adresy i porty każdego z "komputerów" (procesów)
-addresses = ['192.168.0.245', '192.168.0.163', '192.168.0.10']
-ports = [5000, 5001, 5002]
 
 
 class RicartAgrawala:
-    def __init__(self, my_id, total_processes):
+    def __init__(self, my_id, addresses, ports, gui):
         self.my_id = my_id
-        self.total_processes = total_processes
+        self.total_processes = len(addresses)
+        self.addresses = addresses
+        self.ports = ports
         self.clock = 0
 
         self.reply_received = 0
@@ -24,24 +17,28 @@ class RicartAgrawala:
         self.lock = threading.Lock()
         self.queue_lock = threading.Lock()
 
-        self.sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(total_processes)]
-        for i in range(total_processes):
-            self.sockets[i].bind((addresses[self.my_id], ports[i]))
+        self.gui = gui
+
+        self.sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(self.total_processes)]
+        for i in range(self.total_processes):
+            print(i, self.addresses[self.my_id], self.ports[i])
+            self.sockets[i].bind((self.addresses[self.my_id], self.ports[i]))
             threading.Thread(target=self.listen, args=(i,)).start()
 
-    def request_cs(self):
-        self.reply_received = 0
-        self.broadcast_request()
+    def request_cs(self, number_of_requests):
+        for _ in range(int(number_of_requests)):
+            self.reply_received = 0
+            self.broadcast_request()
 
-        while self.reply_received < self.total_processes - 1:
-            pass
+            while self.reply_received < self.total_processes - 1:
+                pass
 
-        while min(self.queue, key=lambda x: x[1])[0] != self.my_id:
-            pass
+            while min(self.queue, key=lambda x: x[1])[0] != self.my_id:
+                pass
 
-        print(f'QUEUE: {self.queue}')
+            print(f'QUEUE: {self.queue}')
 
-        self.enter_cs()
+            self.enter_cs()
 
     def broadcast_request(self):
         self.lock.acquire()
@@ -50,6 +47,7 @@ class RicartAgrawala:
         self.queue.append((self.my_id, self.clock))
         self.queue_lock.release()
 
+        self.clock += 1
         for i in range(self.total_processes):
             if i != self.my_id:
                 self.send_request(i)
@@ -57,28 +55,30 @@ class RicartAgrawala:
         self.lock.release()
 
     def send_request(self, to):
-        self.clock += 1
         message = f"REQUEST:{self.my_id}:{self.clock}"
-        print(f"[{self.my_id}] - {self.clock} - Send REQUEST: {to}")
-        self.sockets[to].sendto(message.encode('utf-8'), (addresses[to], ports[self.my_id]))
+
+        log = f"Clock: {self.clock} - Send REQUEST: {to}"
+        self.gui.data.insert('end', log)
+        self.gui.data.itemconfig(len(self.gui.data.get(0, 'end')) - 1, {'bg': '#F0DA32'})
+
+        self.sockets[to].sendto(message.encode('utf-8'), (self.addresses[to], self.ports[self.my_id]))
 
     def listen(self, from_id):
-        print(f"[{self.my_id}] - {self.clock} - LISTEN: {from_id}")
+        self.gui.log_listen(self.clock, from_id)
         while True:
             data, addr = self.sockets[from_id].recvfrom(1024)
             if data:
                 decoded_data = data.decode('utf-8')
-
                 if decoded_data.startswith('RELEASE:'):
-                    print(f"[{self.my_id}] - {self.clock + 1} - Receive RELEASE: {from_id}")
+                    self.gui.log_receive(self.clock, from_id, "RELEASE")
                     _, sender_id = decoded_data.split(':')
                     self.receive_release(int(sender_id))
                 elif decoded_data.startswith("REQUEST:"):
-                    print(f"[{self.my_id}] - {self.clock + 1} - Receive REQUEST: {from_id}")
+                    self.gui.log_receive(self.clock, from_id, "REQUEST")
                     _, sender_id, sender_clock = decoded_data.split(':')
                     self.receive_request(int(sender_id), int(sender_clock), addr)
                 elif decoded_data.startswith("REPLY:"):
-                    print(f"[{self.my_id}] - {self.clock + 1} - Receive REPLY: {from_id}")
+                    self.gui.log_receive(self.clock, from_id, "REPLY")
                     self.receive_reply()
 
     def receive_request(self, from_id, timestamp, addr):
@@ -110,17 +110,24 @@ class RicartAgrawala:
         self.queue_lock.release()
 
     def send_reply(self, to, addr):
-        self.clock += 1
+        # self.clock += 1
         message = f"REPLY:{self.my_id}"
-        print(f"[{self.my_id}] - {self.clock} - Send REPLY: {to}")
+        # self.gui.log_send_reply(self.clock, to)
+
+        log = f"Clock: {self.clock} - Send REPLY: {to}"
+        print(log)
+        self.gui.data.insert('end', log)
+        self.gui.data.itemconfig(len(self.gui.data.get(0, 'end')) - 1, {'bg': '#3894F0'})
+
         self.sockets[to].sendto(message.encode('utf-8'), addr)
 
     def enter_cs(self):
         self.lock.acquire()
         self.clock += 1
-        print(f"[{self.my_id}] - {self.clock} - ENTER CS")
+        self.gui.log_cs(self.clock, "ENTER")
         time.sleep(5)
-        print(f"[{self.my_id}] - {self.clock} - EXIT CS")
+        self.clock += 1
+        self.gui.log_cs(self.clock, "EXIT")
 
         for i in range(self.total_processes):
             if i != self.my_id:
@@ -132,22 +139,7 @@ class RicartAgrawala:
         self.queue_lock.acquire()
 
         message = f"RELEASE:{self.my_id}"
-        self.sockets[to].sendto(message.encode('utf-8'), (addresses[to], ports[self.my_id]))
+        self.sockets[to].sendto(message.encode('utf-8'), (self.addresses[to], self.ports[self.my_id]))
         self.queue = [item for item in self.queue if item[0] != self.my_id]
 
         self.queue_lock.release()
-
-
-def main(my_id, total_processes, total_request):
-    total_processes = len(addresses)
-    processes = []
-
-    # Tworzymy wątki reprezentujące różne procesy
-    ra = RicartAgrawala(int(my_id), int(total_processes))
-    time.sleep(3)
-    for i in range(int(total_request)):
-        ra.request_cs()
-
-
-if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
